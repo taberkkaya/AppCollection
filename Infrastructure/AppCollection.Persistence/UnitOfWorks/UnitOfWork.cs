6406ffa97c34aefc79bcaebc2c2ab61ef3b9f634
taberkkaya <taberkkaya@gmail.com>
2024-05-20 20:58:15 +0300

    UnitOfWork done

---
+++ Infrastructure/AppCollection.Persistence/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using AppCollection.Application.Interfaces.Repositories;
+using AppCollection.Application.Interfaces.UnitOfWorks;
+using AppCollection.Persistence.Context;
+using AppCollection.Persistence.Repositories;
+
+namespace AppCollection.Persistence.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext dbContext;
+
+        public UnitOfWork(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async ValueTask DisposeAsync() => await dbContext.DisposeAsync();
+        public int SaveChanges() => dbContext.SaveChanges();
+        public Task<int> SaveChangesAsync() => dbContext.SaveChangesAsync();
+        IReadRepository<T> IUnitOfWork.GetReadRepository<T>() => new ReadRepository<T>(dbContext);
+        IWriteRepository<T> IUnitOfWork.GetWriteRepository<T>() => new WriteRepository<T>(dbContext);
+    }
+}
